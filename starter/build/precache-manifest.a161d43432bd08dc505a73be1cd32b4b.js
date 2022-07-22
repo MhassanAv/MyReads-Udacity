c0dfcbf119268a4fe5a04b27400dfdbde23f37f0
MhassanAv <93055463+MhassanAv@users.noreply.github.com>
2022-07-22 13:40:39 +0200

    build

---
+++ starter/build/precache-manifest.a161d43432bd08dc505a73be1cd32b4b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ff142e7d484d639d077",
+    "url": "/static/css/main.0d2cc08e.chunk.css"
+  },
+  {
+    "revision": "9ff142e7d484d639d077",
+    "url": "/static/js/main.9ff142e7.chunk.js"
+  },
+  {
+    "revision": "d4086321369a3bfa0327",
+    "url": "/static/js/1.d4086321.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "2e7d51f7b55b76ffe570a94d164d8bf6",
+    "url": "/index.html"
+  }
+];
