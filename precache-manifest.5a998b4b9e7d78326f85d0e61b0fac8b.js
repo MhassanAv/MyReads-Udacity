54b59113601444b3ef4f2fc4e80eba9b63ae8479
MhassanAv <93055463+MhassanAv@users.noreply.github.com>
2022-07-23 03:24:54 +0200

    Updates

---
+++ precache-manifest.5a998b4b9e7d78326f85d0e61b0fac8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a4029b94d24bf4194b7",
+    "url": "/MyReads-Udacity/static/css/main.86ae0b9b.chunk.css"
+  },
+  {
+    "revision": "6a4029b94d24bf4194b7",
+    "url": "/MyReads-Udacity/static/js/main.6a4029b9.chunk.js"
+  },
+  {
+    "revision": "d4086321369a3bfa0327",
+    "url": "/MyReads-Udacity/static/js/1.d4086321.chunk.js"
+  },
+  {
+    "revision": "d182a2eb969140ef4d6c",
+    "url": "/MyReads-Udacity/static/js/runtime~main.d182a2eb.js"
+  },
+  {
+    "revision": "3d1a645ff08d9740b3e1f4cf0ccc93c5",
+    "url": "/MyReads-Udacity/static/media/arrow-back.3d1a645f.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads-Udacity/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads-Udacity/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "623cb1028a06a47cee6e9cf1f9086661",
+    "url": "/MyReads-Udacity/index.html"
+  }
+];
